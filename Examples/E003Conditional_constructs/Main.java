27414297de2dfc571114d310845d9c7725da794d
vdudn <dudnikvolodumur@gmail.com>
2022-01-11 13:42:57 +0200

    Dey 11-01-22 Loop

---
+++ Examples/E003Conditional_constructs/Main.java
@@ -0,0 +1,13 @@
+package Examples.E003Conditional_constructs;
+
+/*
+Short form of conditional operator
+ */
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 4;
+        if(num1>num2) System.out.println("Первое число больше второго");
+        // Java_10_Code_Convention(r) slide-14
+    }
+}
